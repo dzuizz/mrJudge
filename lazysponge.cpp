79b0713099434b5367142e2b1e70fc436efa025a
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-15 14:13:24 +0800

    Solved Lazy sponge

---
+++ lazysponge.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n, x, e;
+    cin >> n >> x;
+    for(int i=0;i<x;i++){
+        cin >> e;
+    }
+    cout << e;
+}
