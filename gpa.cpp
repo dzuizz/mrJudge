7bce518fc990a376f8ab43ce9fd9b16b8b5f2b9f
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-15 15:19:42 +0800

    Solved gpa(using cout)

---
+++ gpa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int a, n=40;
+    double pt=0.8;
+    cin >> a;
+    if(a>79) cout << "4.0\n";
+    else if(a>69) cout << "3.6\n";
+    else{
+        while(true){
+            if(a<n){
+                cout << fixed << setprecision(1) << pt << endl;
+                break;
+            }
+            n += 5;
+            pt += 0.4;
+        }
+    }
+}
