56300e8113789c1ab5af686ba7e228af3ef44c76
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-15 21:05:34 +0800

    Solved gcd(with modulo)

--- gcd.cpp
+++ gcd.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int gcd(int x, int y){
+    if(y==0)
+        return x;
+    return gcd(y, x%y);
+}
+
 int main(){
     int a, b, c;
     cin >> a >> b;
-    while(a != b){
+    /*while(a != b){
         c = max(a, b) - min(a, b);
         a = min(a, b);
         b = c;
-    }
-    cout << a << endl;
+    }*/
+    cout << gcd(a, b) << endl;
 }
