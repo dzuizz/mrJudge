7025015b95ae67d2edfe51a85c80da8031409b9f
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-15 21:57:15 +0800

    Solved paint (the easier way with array)

--- paint.cpp
+++ paint.cpp
@@ -4,21 +4,36 @@
 using namespace std;
 
 int main(){
-    int n, e, total=0;
-    vector<int> price, increment;
+    int n, total=0;
+    int price[20000], increment[20000];
+
+    //vector<int> price, increment;
     cin >> n;
     for(int i=0;i<n;i++){
-        cin >> e;
-        price.push_back(e);
+        cin >> price[i];
+
+        //price.push_back(e);
     } for(int i=0;i<n;i++){
-        cin >> e;
-        increment.push_back(e);
+        cin >> increment[i];
+
+        //increment.push_back(e);
     }for(int i=0;i<n;i++){
-        vector<int>::iterator it = max_element(increment.begin(), increment.end());
+        int e=-1, ind=-1;
+        for(int j=0;j<n;j++){
+            if(increment[j]>e){
+                e = increment[j];
+                ind = j;
+            }
+        }
+        total += price[ind] + e*i;
+        price[ind] = -1;
+        increment[ind] = -1;
+
+        /*vector<int>::iterator it = max_element(increment.begin(), increment.end());
         vector<int>::iterator it2 = next(price.begin(), it - increment.begin());
         total += *it2 + *it * i;
         price.erase(it2);
-        increment.erase(it);
+        increment.erase(it);*/
     }
     cout << total << endl;
 }
