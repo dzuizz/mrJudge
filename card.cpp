8c12688093bb7b04b7d7072029ff3cb41f941f7c
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-18 23:00:29 +0800

    Solved card with linked list 100/100

--- card.cpp
+++ card.cpp
@@ -2,9 +2,48 @@
 #include <vector>
 using namespace std;
 
+class Card {
+public:
+    int data;
+    Card* next;
+};
+
 int main(){
     int m, k;
-    vector<int> cards;
+    char move = '|';
+    scanf("%d %d", &m, &k);
+    Card* head = NULL;
+    Card* tail = NULL;
+    for(int i=m-1;i>=0;i--){
+        Card* card = new Card();
+        card->data = i;
+        card->next = head;
+        head = card;
+        if(i==m-1){
+            tail = card;
+        }
+    }
+    while(move != '.'){
+        scanf("%c", &move);
+        if(move=='A'){
+            tail->next = head;
+            tail = head;
+            head = head->next;
+            tail->next = NULL;
+        }else if(move=='B'){
+            Card* second = head->next;
+            tail->next = second;
+            head->next = second->next;
+            second->next = NULL;
+            tail = tail->next;
+        }
+    }
+    for(int i=0;i<k-1;i++){
+        head = head->next;
+    }
+    printf("%d %d %d", head->data, head->next->data, head->next->next->data);
+
+    /*vector<int> cards;
     char move='h';
     scanf("%d %d", &m, &k);
     for(int i=0;i<m;i++){
@@ -20,5 +59,5 @@ int main(){
             cards.erase(cards.begin()+1);
         }
     }
-    printf("%d %d %d", cards[k-1], cards[k], cards[k+1]);
+    printf("%d %d %d", cards[k-1], cards[k], cards[k+1]);*/
 }
